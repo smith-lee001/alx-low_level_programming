48c993849213258c1962a2e1cf0b0a23911a4ffe
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-17 00:53:48 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Program to print the alphabet in lowercase, followed by a new line.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for(letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
