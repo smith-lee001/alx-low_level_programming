fdf7739e2c178ba2ee8cdf7833569ef6711b7f76
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-18 01:19:53 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Code that prints all possible diff combinations of two digits
+ *using putchar
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int numb1 = 0, numb2;
+while (numb1 <= 9)
+{
+numb2 = 0;
+while (numb2 <= 9)
+{
+if (numb1 != numb2 && numb1 < numb2)
+{
+putchar(numb1 + 48);
+putchar(numb2 + 48);
+if (numb1 + numb2 != 17)
+}
+putchar(',');
+putchar(' ');
+}
+}
+++numb2;
+}
+++numb1;
+}
+putchar('\n');
+return (0);
+}
+
