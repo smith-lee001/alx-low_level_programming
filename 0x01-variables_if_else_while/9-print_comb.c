802141c0d75107b96d2b00d9f075d9252f95b679
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-18 00:42:31 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Code that prints possible combinations of single digits
+ *using putchar
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int numb = 0;
+while (numb <= 9)
+{
+putchar(',');
+putchar('')
+}
+++numb;
+}
+putchar('\n');
+return (0);
+}
+
