d45c23c8e2e5f6a0dee812012bbc8c1656820fe4
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-18 04:35:43 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *Code that prints all possible diff combinations of three digits
- *using putchar
+ * Code that prints all possible different combinations of three digits
+ * using putchar
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
@@ -15,23 +15,24 @@ numb2 = 0;
 while (numb2 <= 9)
 {
 num3 = 0;
-if (num3 <= 9)
+while (num3 <= 9)
 {
 if (numb1 != numb2 && numb1 < numb2 && numb2 != num3 && numb2 < num3)
-putchar(numb1 + 48);
-putchar(numb2 + 48);
-putchar(num3 + 48);
+{
+putchar(numb1 + '0');
+putchar(numb2 + '0');
+putchar(num3 + '0');
 if (numb1 + numb2 + num3 != 24)
-}
+{
 putchar(',');
 putchar(' ');
 }
 }
-++num3;
+num3++;
 }
-++numb2;
+numb2++;
 }
-++numb1;
+numb1++;
 }
 putchar('\n');
 return (0);
