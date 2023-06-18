ef74b26c3fe097ff13bbc49e5e12b149443f6b47
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-18 00:01:53 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Code that prints digits of base 10 starting zero
+ *using putchar
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int numb = 0;
+while (numb <= 9)
+{
+putchar(numb + '0');
+++numb;
+}
+printf("\n");
+return (0);
+}
+
