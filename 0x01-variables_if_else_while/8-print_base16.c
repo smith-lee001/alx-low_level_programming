61d0002e29d00973cf270d4dd8b93632d0a8426b
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-18 00:33:00 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Code that prints digits of base 16 in lowercase
+ *using putchar
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int numb = 48; /*48:decimal rep of '0'*/
+while (numb <= 102) /*102;decimal rep of 'f'*/
+{
+putchar(numb);
+if (numb == 57)
+numb += 39;
+}
+putchar('\n');
+return (0);
+}
+
