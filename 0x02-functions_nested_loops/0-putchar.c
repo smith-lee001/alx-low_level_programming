826f2ae79eec6e5f2d9db73aa56239f49c22bb60
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-21 13:30:08 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Code that prints _putchar
+ * followed by new line
+ * Return 0 (Success)
+ */
+int main(void)
+{
+putchar("_putchar", \n);
+return (0);
+}
