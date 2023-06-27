e0fcb01d10a412ad8b9003a7c4893e7f0098d0bb
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-27 14:42:50 -0700

    TAsk 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * char *_strcpy - finction that copies string
+ * @dest: copy to
+ * @src: copy from
+ * return: string
+ */
+char _strcpy(char *dest, char *src)
+{
+int l;
+int x;
+while (*(src + l) != '\0')
+{
+l++;
+}
+for ( ; x < l; x++)
+{
+dest[x] = src[x];
+}
+dest[l] = '\0';
+return (dest);
+}
