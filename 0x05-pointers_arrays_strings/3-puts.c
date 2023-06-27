1d8e4bc11b04ae3b619bc05742a19f30c3e43c0b
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-27 12:22:56 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: The input string.
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}
+
