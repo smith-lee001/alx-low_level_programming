88f703a089e6b9d490a1b2694728a2d9354ec51a
smith-lee001 <leesmithmwangi@gmail.com>
2023-06-27 16:02:02 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- * return: the integer converted from the string
+ * return: the integer converted from the string or 0 if no reason
  */
 int _atoi(char *s)
 {
