3f63de2b7019b1db8b1268ebca3392b6cfcdb828
smith-lee001 <leesmithmwangi@gmail.com>
2023-07-05 13:24:07 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion -  returns the value of x raised to power of y
  * @x: value to raise
  * @y: power
- * return: result of power of y or -1 if y is negative
+ * Return: result of power of y or -1 if y is negative
  */
 int _pow_recursion(int x, int y)
 {
