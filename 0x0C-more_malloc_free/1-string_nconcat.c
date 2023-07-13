182b547f44c8ccf464b325ce5eb88c50d26cd2b8
smith-lee001 <leesmithmwangi@gmail.com>
2023-07-13 12:40:43 -0700

    Trust the process

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,10 +26,11 @@ while (i < len1)
 s[i] = s1[i];
 i++;
 }
-while (n < len2 && i < (len1 + n))
-s[i++] = s2[j++];
-while (n < len2 && i < (len1 + len2))
-s[i++] = s2[j++];
+while (n < len2 && j < n)
+s[i] = s2[j];
+i++;
+j++;
+}
 s[i] = '\0';
 return (s);
 }
